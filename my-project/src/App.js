ebc44596edf98b916514e75e23cd42ff5a898d24
Shriram Prajapati <shriram.prajapati@supersoucing.com>
2024-07-10 22:32:26 +0530

    error handling

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,13 +1,12 @@
-import { useEffect } from "react";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [countries, setCountries] = useState([]);
+
   useEffect(() => {
     const fetchCountries = async () => {
       try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
+        const response = await fetch("https://xcountriesapi.onrender.com/all");
         const data = await response.json();
         setCountries(data);
       } catch (err) {
@@ -18,7 +17,7 @@ export default function App() {
   }, []);
 
   const cardStyle = {
-    Width: "200px",
+    width: "200px",
     border: "1px solid #ccc",
     margin: "10px",
     padding: "10px",
@@ -37,20 +36,20 @@ export default function App() {
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "center",
-    alignItem: "center",
+    alignItems: "center",
     height: "100vh",
   };
 
   return (
     <div style={containerStyle}>
       {countries.map((country) => (
-        <div key={country} style={cardStyle}>
+        <div key={country.abbr} style={cardStyle}>
           <img
-            src={country.flags.png}
-            alt={`Flag of ${country.name.common}`}
+            src={country.flag}
+            alt={`Flag of ${country.name}`}
             style={imgStyle}
           />
-          <h2>{country.name.common}</h2>
+          <h2>{country.name}</h2>
         </div>
       ))}
     </div>
