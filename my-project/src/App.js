b4da84aecc0c15a1e3fd85b86c2c8f7b8f378a66
Shriram Prajapati <shriram.prajapati@supersoucing.com>
2024-07-10 22:15:45 +0530

    error handling

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
 
 export default function App() {
   const [countries, setCountries] = useState([]);
-
   useEffect(() => {
     const fetchCountries = async () => {
       try {
-        const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
+        const response = await fetch("https://restcountries.com/v3.1/all");
         const data = await response.json();
         setCountries(data);
       } catch (err) {
@@ -17,9 +18,8 @@ export default function App() {
   }, []);
 
   const cardStyle = {
-    width: "200px",
+    Width: "200px",
     border: "1px solid #ccc",
-    borderRadius: "10px",
     margin: "10px",
     padding: "10px",
     display: "flex",
@@ -28,7 +28,7 @@ export default function App() {
     justifyContent: "center",
   };
 
-  const imageStyle = {
+  const imgStyle = {
     width: "100px",
     height: "100px",
   };
@@ -37,25 +37,22 @@ export default function App() {
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "center",
-    alignItems: "center",
+    alignItem: "center",
     height: "100vh",
   };
-  const textCenter = {
-    textAlign:"center"
-  }
 
   return (
     <div style={containerStyle}>
       {countries.map((country) => (
-        <div key={country.abbr} style={cardStyle}>
+        <div key={country} style={cardStyle}>
           <img
-            src={country.flag}
-            alt={`Flag of ${country.name}`}
-            style={imageStyle}
+            src={country.flags.png}
+            alt={`Flag of ${country.name.common}`}
+            style={imgStyle}
           />
-          <h2 style={textCenter}>{country.name}</h2>
+          <h2>{country.name.common}</h2>
         </div>
       ))}
     </div>
   );
-}
+}ZZZ
